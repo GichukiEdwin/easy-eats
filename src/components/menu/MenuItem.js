705439f4bf3f856a045b15028f087b32e83eef02
edugichuki <muriithigichuki854@gmail.com>
2024-09-05 22:40:41 +0300

    Add item tile pop up

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,31 +1,39 @@
 import { CartContext } from "@/context/Provider";
 import Image from "next/image";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import toast from "react-hot-toast";
+import MenuItemTile from "./MenuItemTile";
 export const MenuItem = (menuItem) => {
   const { image, name, description, basePrice, sizes, extraIngredientsPrices } =
     menuItem;
+  const [showPopUp, setShowPopUp] = useState(false);
   const { addToCart } = useContext(CartContext);
+
+  const handleAddToCartButtonClick = () => {
+    if (sizes.length === 0 && extraIngredientsPrices.length === 0) {
+      addToCart(menuItem);
+      toast.success("Added to cart");
+      setShowPopUp(true);
+    } else {
+    }
+  };
+
   return (
     <>
-      <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white transition-all hover:shadow-md hover:shadow-black/25">
-        <div className="text-center">
-          <Image
-            src={image}
-            alt={"pizza"}
-            width={"150"}
-            height={"200"}
-            className="max-h-auto max-h-24 mx-auto block"
-          />
+      {showPopUp && (
+        <div className="fixed inset-0 bg-black/80 flex items-center justify-center">
+          <div className="bg-white p-4 rounded-lg">
+            <Image
+              src={image}
+              alt={"pizza"}
+              width={"200"}
+              height={"200"}
+              className="max-h-auto max-h-24 mx-auto block"
+            />
+          </div>
         </div>
-        <h4 className="font-semibold my-2 text-xl">{name}</h4>
-        <p className="text-gray-600 text-sm line-clamp-3">{description}</p>
-        <button
-          className="bg-primary text-white px-6 py-2 mt-4 rounded-full"
-          onClick={() => addToCart(menuItem)}
-        >
-          Add to cart ${basePrice}
-        </button>
-      </div>
+      )}
+      <MenuItemTile onAddToCart={handleAddToCartButtonClick} {...menuItem} />
     </>
   );
 };
---
+++ src/components/menu/MenuItemTile.js
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+export default function MenuItemTile({ onAddToCart, ...item }) {
+  const { image, description, name, basePrice } = item;
+
+  return (
+    <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white transition-all hover:shadow-md hover:shadow-black/25">
+      <div className="text-center">
+        <Image
+          src={image}
+          alt={"pizza"}
+          width={"150"}
+          height={"200"}
+          className="max-h-auto max-h-24 mx-auto block"
+        />
+      </div>
+      <h4 className="font-semibold my-2 text-xl">{name}</h4>
+      <p className="text-gray-600 text-sm line-clamp-3">{description}</p>
+      <button
+        type="button"
+        className="bg-primary text-white px-6 py-2 mt-4 rounded-full"
+        onClick={onAddToCart}
+      >
+        Add to cart ${basePrice}
+      </button>
+    </div>
+  );
+}
