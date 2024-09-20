cac92d06b232896e3e076fc5b84d0d5fa9069217
edugichuki <muriithigichuki854@gmail.com>
2024-09-20 21:27:36 +0300

    Update

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -26,10 +26,30 @@ export const MenuItem = (menuItem) => {
             <Image
               src={image}
               alt={"pizza"}
-              width={"200"}
+              width={"150"}
               height={"200"}
               className="max-h-auto max-h-24 mx-auto block"
             />
+            <h4 className="font-semibold my-2 text-xl">{name}</h4>
+            <p className="text-gray-600 text-sm line-clamp-3">{description}</p>
+            {/* {sizes?.length > 0 && (
+              <div className="bg-gray-200 p-4 rounded-lg">
+                <h4 className="font-semibold my-2 text-xl">Pick a size</h4>
+                {sizes.map((size) => (
+                  <label key={size}>
+                    <input type="radio" />
+                    {size.name} ${basePrice + size.price}
+                  </label>
+                ))}
+              </div>
+            )} */}
+            <button
+              type="button"
+              className="bg-primary text-white px-6 py-2 mt-4 rounded-full"
+              onClick={() => setShowPopUp(false)}
+            >
+              Close
+            </button>
           </div>
         </div>
       )}
