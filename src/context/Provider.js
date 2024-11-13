54f88257b44292ec0c50bdf1ff3cbe5b30cbc116
Muriithi Gichuki <107644777+GichukiEdwin@users.noreply.github.com>
2024-11-13 10:39:51 +0300

    Edit function to remove from cart

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -3,9 +3,24 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import { createContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const queryClient = new QueryClient();
 export const CartContext = createContext({});
+
+export function cartProductPrice(cartProduct) {
+  let price = cartProduct.basePrice;
+  if (cartProduct.size) {
+    price += cartProduct.size.price;
+  }
+  if (cartProduct.extras?.length > 0) {
+    for (const extra of cartProduct.extras) {
+      price += extra.price;
+    }
+  }
+  return price;
+}
+
 export const Provider = ({ children }) => {
   const [cartProducts, setCartProducts] = useState([]);
   const ls = typeof window !== "undefined" ? window.localStorage : null;
@@ -21,14 +36,15 @@ export const Provider = ({ children }) => {
     saveCartProductsToLocalStorage([]);
   };
 
-  const removeFromCart = (productId) => {
+  const removeFromCart = (indexToRemove) => {
     setCartProducts((prevProducts) => {
       const newProducts = prevProducts.filter(
-        (product) => product._id !== productId
+        (v, index) => index !== indexToRemove
       );
       saveCartProductsToLocalStorage(newProducts);
       return newProducts;
     });
+    toast.success("Product removed");
   };
 
   const saveCartProductsToLocalStorage = (cartProducts) => {
